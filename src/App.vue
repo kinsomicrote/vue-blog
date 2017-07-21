a6cd5770ffe0bddaa66ed63d43a7a628d1f8824c
kinsomicrote <kinsomicrote.inc@gmail.com>
2017-07-21 19:50:58 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <show-blogs></show-blogs>
+    <!--<add-blog></add-blog>-->
+  </div>
+</template>
+
+<script>
+import addBlog from './components/addBlog'
+import showBlogs from './components/showBlogs'
+
+export default {
+  components: {
+    'add-blog': addBlog,
+    'show-blogs': showBlogs
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: 'Nunito SemiBold';
+}
+</style>
